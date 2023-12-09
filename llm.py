cc528b870e39d5c1cb19306bc313d82be421ecf3
Phuoc Nguyen <pngu0075@student.monash.edu>
2023-12-09 16:19:17 +1100

    llm model not working

--- llm.py
+++ llm.py
@@ -3,29 +3,32 @@ from apikey import apikey
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from flask import request
 
-def generate():
-    ingredients = input("Insert your ingredients here")
-    cuisine = input("What kind of cuisine would you like ?")
+ingredients = input("enter your ingredients: ")
+cuisine = input ("enter your cuisine: ")
+#Prompt templates
+title_template = PromptTemplate(
+                input_variables= ['ingredients', 'cuisine'],
+                template = 'Give me a name of dish made out from {ingredients} in {cuisine} cuisine'
+            )
 
-    #Prompt templates
-    title_template = PromptTemplate(
-            input_variables= ['ingredients', 'cuisine'],
-            template = 'Give me a name of dish made out from {ingredients} in {cuisine} cuisine'
-        )
+instructions_template = PromptTemplate(
+                input_variables= ['title'],
+                template = 'Give me instructions how to make this title TITLE:{title}'
+            )
+        #LLMS
+llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, output_key='title')
+instructions_chain = LLMChain(llm=llm, prompt=instructions_template, output_key='instructions')
+sequential_chain = SequentialChain(chains=[title_chain,instructions_chain], input_variables=['ingredients','cuisine'], output_variables=['title','instructions'], verbose=True)
 
-    instructions_template = PromptTemplate(
-            input_variables= ['title'],
-            template = 'Give me instructions how to make this title TITLE:{title}'
-        )
-    #LLMS
-    llm = OpenAI(temperature=0.9)
-    title_chain = LLMChain(llm=llm, prompt=title_template, output_key='title')
-    instructions_chain = LLMChain(llm=llm, prompt=instructions_template, output_key='instructions')
-    sequential_chain = SequentialChain(chains=[title_chain,instructions_chain], input_variables=['ingredients','cuisine'], output_variables=['title','instructions'], verbose=True)
-
-    '"give response if there is an input"'
-    if ingredients: 
+'"give response if there is an input"'
+if ingredients: 
         response = sequential_chain({'ingredients':ingredients, 'cuisine':cuisine})
         print(response['title'])
         print(response['instructions'])
+          
+        
+
+   
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 import os 
 from apikey import apikey
@@ -14,21 +14,22 @@ views = Blueprint('views', __name__)
 def welcome():
     return render_template('welcome.html')
 
+
+
 @views.route('/create', methods = ['POST', 'GET'])
-@login_required
 def create():
     if request.method == ['POST']:
-        ingredients = request.form.get('ingredients')
+        ingredients= request.form.get('ingredients')
         cuisine = request.form.get('cuisine')
-        
+                   
         title_template = PromptTemplate(
-            input_variables= ['ingredients', 'cuisine'],
-            template = 'Give me a name of dish made out from {ingredients} in {cuisine} cuisine'
-        )
+                input_variables= ['ingredients', 'cuisine'],
+                template = 'Give me a name of dish made out from {ingredients} in {cuisine} cuisine'
+            )
 
         instructions_template = PromptTemplate(
-                input_variables= ['title'],
-                template = 'Give me instructions how to make this title TITLE:{title}'
+                        input_variables= ['title'],
+                        template = 'Give me instructions how to make this title TITLE:{title}'
             )
         #LLMS
         llm = OpenAI(temperature=0.9)
@@ -36,13 +37,13 @@ def create():
         instructions_chain = LLMChain(llm=llm, prompt=instructions_template, output_key='instructions')
         sequential_chain = SequentialChain(chains=[title_chain,instructions_chain], input_variables=['ingredients','cuisine'], output_variables=['title','instructions'], verbose=True)
 
-        '"give response if there is an input"'
+        #give response if there is an input
         if ingredients: 
             response = sequential_chain({'ingredients':ingredients, 'cuisine':cuisine})
-            
             new_recipe = Recipe(title= response['title'], instructions= response['instructions'], user_id=current_user.id )
             db.session.add(new_recipe)
             db.session.commit()
-    
+            
         
+    
     return render_template('create.html', user=current_user)
